eb7d1258229d7d10bdb2e3b3e6f849db8a759f1a
Rubenia Borge <rubeniaolaska@gmail.com>
2022-07-27 09:49:46 -0500

    Update Pig.cpp

--- Pig.cpp
+++ Pig.cpp
@@ -1,1 +1,28 @@
 
+#include "Pig.h"
+
+void Pig::SetBreed(string pBreed) {
+	Breed = pBreed;
+}
+void Pig::SetSpayedNeutered(string pSpayedNeutered) {
+	SpayedNeutered = pSpayedNeutered;
+}
+void Pig::SetRegistrationID(string pRegistrationID) {
+	RegistrationID = pRegistrationID;
+}
+void Pig::SetComments(string pComments) {
+	Comments = pComments;
+}
+
+string Pig::GetBreed() {
+	return Breed;
+}
+string Pig::GetSpayedNeutered() {
+	return SpayedNeutered;
+}
+string Pig::GetRegistrationID() {
+	return RegistrationID;
+}
+string Pig::GetComments() {
+	return Comments;
+}
